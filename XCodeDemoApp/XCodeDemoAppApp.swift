1f6b4b058efd2ad88c88fc6783c49f39c4addc7c
sougatoroy3 <sougatoroy007@gmail.com>
2025-01-26 01:23:08 +0530

    Initial Commit

---
+++ XCodeDemoApp/XCodeDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeDemoAppApp.swift
+//  XCodeDemoApp
+//
+//  Created by Sougato Roy on 26/01/25.
+//
+
+import SwiftUI
+
+@main
+struct XCodeDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XCodeDemoAppTests/XCodeDemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeDemoAppTests.swift
+//  XCodeDemoAppTests
+//
+//  Created by Sougato Roy on 26/01/25.
+//
+
+import Testing
+@testable import XCodeDemoApp
+
+struct XCodeDemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
